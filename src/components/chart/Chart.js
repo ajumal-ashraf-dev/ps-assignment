e21f4f0d6be9b555b081b54ff03ad0a942e2f888
Ajumal Ashraf <ajumal100@hotmail.com>
2020-06-15 03:52:59 +0530

    Charts Component Completed

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+ 
 import './Chart.scss';
 
 const Chart = () => {
     const newsList = useSelector(state => state.news.list);
-    return <div></div>
+    
+    const options = {
+        title: {
+          text: ''
+        },
+        chart: {
+            backgroundColor: '#f6f6ef'
+        },
+        yAxis: {
+            title: {
+                text: 'Votes',
+                style: {
+                    fontSize: '15px',
+                    fontWeight: 'bold'
+                }
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'ID',
+                style: {
+                    fontSize: '15px',
+                    fontWeight: 'bold'
+                }
+            },
+            categories: newsList.map(obj => obj.objectID)
+        },
+        series: [{
+          data: newsList.map(obj => obj.points),
+          showInLegend: false,
+          name: "Votes"
+        }]
+    }
+    return <div className="chart-container">
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={options}
+        />
+    </div>
 }
 
 export default Chart;
